d35a14ff2717b0e702e2aeb4c0d6ff68f8505c43
gbrl-mendes <gabrielmendesbrt@gmail.com>
2022-04-12 17:09:39 -0300

    alterações

--- script/lagoa_plots_reanalise_08abr22.R
+++ script/lagoa_plots_reanalise_08abr22.R
@@ -4,7 +4,7 @@ library(ggplot2)
 #Obtencao dos dados
 {
   raw_results_tbl <- read.csv("~/projetos/lagoa_ingleses/tabelas/raw/08mar22/v3_ed_li_22mar22-todas_info_da_analise_2022-04-08.csv",
-                              sep = ";", header = T, check.names = F, dec = ",")
+                              sep = ";", header = T, dec = ",")
 }
 
 #Criacao da lista com os possiveis nomes atribuidos as ASVs
@@ -31,34 +31,34 @@ library(ggplot2)
       # "exact Species", # "X1_res", # "X1_subject header", # "X1_query", # "X1_subject", # "X1_indentity", # "X1_length", # "X1_mismatches", # "X1_gaps", # "X1_query start", # "X1_query end", # "X1_subject start", # "X1_subject end", # "X1_e value", # "X1_bitscore", # "X1_qcovhsp",
       # "X2_res", # "X2_subject header", # "X2_query", # "X2_subject", # "X2_indentity", # "X2_length", # "X2_mismatches", # "X2_gaps", # "X2_query start", # "X2_query end", # "X2_subject.start", # "X2_subject.end",# "X2_e.value", # "X2_bitscore", # "X2_qcovhsp", # "X3_res", # "X3_subject.header",
       # "X3_query", # "X3_subject", # "X3_indentity", # "X3_length", # "X3_mismatches", # "X3_gaps", # "X3_query.start", # "X3_query.end", # "X3_subject.start", # "X3_subject.end", # "X3_e.value", # "X3_bitscore", # "X3_qcovhsp", # "Relative.abundance.to.all.samples", 
-      "Relative abundance on sample", # "Sample total abundance", #"exact GenSp", #"final ID",
-      "Curated ID", # "Size  pb ", # "ASV header", # "Remove
-    )) %>% group_by(Sample, `Curated ID`, Coleta, Point , `Sample.Name`, File_name) %>%
+      "Relative.abundance.on.sample", # "Sample total abundance", #"exact GenSp", #"final ID",
+      "Curated.ID", # "Size  pb ", # "ASV header", # "Remove
+    )) %>% group_by(Sample, `Curated.ID`, Coleta, Point , `Sample.Name`, File_name) %>%
     summarize(`Sample` = unique(Sample),
-              `Curated ID` = unique(`Curated ID`),
+              `Curated.ID` = unique(`Curated.ID`),
               `Coleta` = unique(Coleta),
               `Ano` = unique(Ano),
               `Point` = unique(Point),
               `Sample.Name` = unique(`Sample.Name`),
               `File_name` = unique(File_name),
-              `RRA` = sum(`Relative abundance on sample`)) %>%
+              `RRA` = sum(`Relative.abundance.on.sample`)) %>%
     ungroup()
 }
 
 #Trocando os nomes dos animais não-peixes
 {
-  grouped_by_ID_tbl$`Curated ID`[grouped_by_ID_tbl$`Curated ID` == "Bos taurus"] <- "Bos taurus (Boi)"
-  grouped_by_ID_tbl$`Curated ID`[grouped_by_ID_tbl$`Curated ID` == "Canis familiaris"] <- "Canis familiaris (Cachorro doméstico)"
-  grouped_by_ID_tbl$`Curated ID`[grouped_by_ID_tbl$`Curated ID` == "Cavia magna"] <- "Cavia magna (Preá)"
-  grouped_by_ID_tbl$`Curated ID`[grouped_by_ID_tbl$`Curated ID` == "Hydrochaeris hydrochaeris"] <- "Hydrochaeris hydrochaeris (Capivara)"
-  grouped_by_ID_tbl$`Curated ID`[grouped_by_ID_tbl$`Curated ID` == "Nannopterum brasilianus"] <- "Nannopterum brasilianus (Biguá)"
-  grouped_by_ID_tbl$`Curated ID`[grouped_by_ID_tbl$`Curated ID` == "Progne chalybea"] <- "Progne chalybea (Andorinha-grande)"
-  grouped_by_ID_tbl$`Curated ID`[grouped_by_ID_tbl$`Curated ID` == "Sus scrofa"] <- "Sus scrofa (Javali)"
+  grouped_by_ID_tbl$`Curated.ID`[grouped_by_ID_tbl$`Curated.ID` == "Bos taurus"] <- "Bos taurus (Boi)"
+  grouped_by_ID_tbl$`Curated.ID`[grouped_by_ID_tbl$`Curated.ID` == "Canis familiaris"] <- "Canis familiaris (Cachorro doméstico)"
+  grouped_by_ID_tbl$`Curated.ID`[grouped_by_ID_tbl$`Curated.ID` == "Cavia magna"] <- "Cavia magna (Preá)"
+  grouped_by_ID_tbl$`Curated.ID`[grouped_by_ID_tbl$`Curated.ID` == "Hydrochaeris hydrochaeris"] <- "Hydrochaeris hydrochaeris (Capivara)"
+  grouped_by_ID_tbl$`Curated.ID`[grouped_by_ID_tbl$`Curated.ID` == "Nannopterum brasilianus"] <- "Nannopterum brasilianus (Biguá)"
+  grouped_by_ID_tbl$`Curated.ID`[grouped_by_ID_tbl$`Curated.ID` == "Progne chalybea"] <- "Progne chalybea (Andorinha-grande)"
+  grouped_by_ID_tbl$`Curated.ID`[grouped_by_ID_tbl$`Curated.ID` == "Sus scrofa"] <- "Sus scrofa (Javali)"
 }
 
 #organizar as especies
 {
-  grouped_by_ID_tbl$`Curated ID` %>% unique()%>% sort() %>%  paste0(collapse = '",\n"') %>% cat()
+  grouped_by_ID_tbl$`Curated.ID` %>% unique()%>% sort() %>%  paste0(collapse = '",\n"') %>% cat()
   grouped_by_ID_tbl$Sample %>% unique()%>% sort() %>%  paste0(collapse = '",\n"') %>% cat()
 }
 
@@ -127,9 +127,9 @@ library(ggplot2)
 #sem os grupos
 {
   grouped_by_ID_tbl <- grouped_by_ID_tbl %>%
-    mutate(Curated ID = factor(Curated ID,
+    mutate(Curated.ID = factor(Curated.ID,
                                levels = rev(c(#"",
-                                 "Astyanax",
+                                 #"Astyanax",
                                  "Astyanax fasciatus",
                                  "Astyanax lacustris",
                                  "Brycon orthotaenia",
@@ -191,25 +191,25 @@ library(ggplot2)
     #transformando variaveis categoricas em fatores com niveis
     mutate(Coleta = factor(Coleta)) %>%
     mutate(Sample = factor(Sample,
-                           levels = c("L1_nov21",
-                                      "L1_out21",
-                                      "L2_nov21",
-                                      "L2_out21",
-                                      "L3_nov21",
-                                      "L3_out21",
-                                      "L4_nov21",
-                                      "L4_out21",
-                                      "L1-neo-mi",
-                                      "L2 Dez/20",
-                                      "L2 Nov/20"
-                                      
-                           ))) %>% mutate(Coleta = factor(Coleta,
-                                                              levels = c("Nov_Dec/20",
-                                                                         "Nov/20",
-                                                                         "Dec/20",
-                                                                         "out/21",
-                                                                         "Nov/21"
-                                                              ))) %>%
+                           levels = c("L1_nov_dec_20_mi", 
+                                      "L1_nov_dec_20_neo", 
+                                      "L1_nov21", 
+                                      "L1_out21", 
+                                      "L2_dez20", 
+                                      "L2_nov20", 
+                                      "L2_nov21", 
+                                      "L2_out21", 
+                                      "L3_nov21", 
+                                      "L3_out21", 
+                                      "L4_nov21", 
+                                      "L4_out21" ))) %>% 
+    mutate(Coleta = factor(Coleta, 
+                           levels = c("Nov_Dec/20",
+                                      "Nov/20",
+                                      "Dec/20",
+                                      "out/21",
+                                      "Nov/21"
+                                      ))) %>%
     mutate(Point = factor(Point)) %>%
     mutate(File_name = factor(File_name)) %>%
     mutate(Coleta = factor(Coleta)) %>%
@@ -250,10 +250,23 @@ library(ggplot2)
          x = "Amostras",
          y= "Espécies") +
     geom_hline(yintercept = c(7.5)) +
-    scale_fill_continuous(type = "viridis") +
+    # scale_fill_continuous(type = "viridis") +
+    # scale_fill_continuous(viridis::turbo(n=9)) +
+    viridis::scale_fill_viridis(
+      alpha = 1,
+      begin = 0.75,
+      end = 0,
+      direction = 1,
+      discrete = FALSE,
+      option = "B"
+    )
     theme(text=element_text(size = 10, face = "bold"))
   
 }
+
+scales::show_col(viridis::turbo(n=9))
+scales::show_col(viridis::inferno(n=9))
+scales::show_col(viridis::magma(n=9))
 
 #Criacao do Tile Plot das amostras da Lagoa dos Ingleses sequenciadas nas corridas 2, 4 e 5
 #exibindo  por ano apenas
