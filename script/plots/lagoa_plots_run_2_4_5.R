a0b2e758669e6aa771074bb65e46b73240eae719
gbrl-mendes <gabrielmendesbrt@gmail.com>
2022-08-15 16:09:52 -0300

    alterações

--- script/plots/lagoa_plots_run_2_4_5.R
+++ script/plots/lagoa_plots_run_2_4_5.R
@@ -793,40 +793,23 @@ library(ggh4x)
               independent ='x'
   )
 
-# Calculo de abundancia relativa de peixes e nao peixes ----
-  options(scipen = 1)
+# Calculo da proporcao de sequencias de peixes e nao-peixes ----
+
+## Utilizando a abundancia relativa nas amostras para calcular a proporcao
   grouped_by_ID_tbl <- raw_results_tbl %>%
     select(c(
       "Sample",
-      # "Run",
-      # "Group",
       "Expedition",
-      # "Coleta",
       "Year",
       "Sample.Name",
       "File_name",
-      # "OTU",
-      # "Read_origin",
       "Curated ID",
-      # "final ID",
-      # "Abundance",
       "Relative abundance to all samples",
       "Relative abundance on sample",
-      # "Sample.total.abundance",
-      # "Type",
-      "Point"
-      # , # "Sub.point", # "Depth", # "Num.replicates", # "Obs", # "Primer", # "Quantidade.de.ovos.ou.larvas", 
-      # "Kingdom", # "Phylum", # "Class", # "Order", # "Family", # "Genus", # "Species", # "Specimen", # "Basin", 
-      # "exact.Genus", # "exact.Species", # "exact.GenSp", # "X1_subject.header", # "X1_subject", # "X1_indentity", 
-      # "X1_length", # "X1_mismatches", # "X1_gaps", # "X1_query.start", # "X1_query.end", # "X1_subject.start", 
-      # "X1_subject.end", # "X1_e.value", # "X1_bitscore", # "X1_qcovhsp", # "X2_subject.header", # "X2_subject", 
-      # "X2_indentity", # "X2_length", # "X2_mismatches", # "X2_gaps", # "X2_query.start", # "X2_query.end", 
-      # "X2_subject.start", # "X2_subject.end", # "X2_e.value", # "X2_bitscore", # "X2_qcovhsp", # "X3_subject.header", 
-      # "X3_subject", # "X3_indentity", # "X3_length", # "X3_mismatches", # "X3_gaps", # "X3_query.start", 
-      # "X3_query.end", # "X3_subject.start", # "X3_subject.end", # "X3_e.value", # "X3_bitscore", # "X3_qcovhsp", 
-      # "Tag.pairs", # "Tag.FWD", # "Tag.REV", # "Control", # "Size..pb.", # "ASV.header", # "ASV..Sequence.", 
-      # "Remove", # "Probable.bacteria", # "Abd..higher.than.in.control"
-    )) %>% 
+      "Point",
+      "Abundance",
+      "Sample total abundance"
+          )) %>% 
     mutate("Peixe" = if_else (`Curated ID` %in% c("",
                                                   "Cutibacterium acnes",
                                                   "Bos taurus",
@@ -840,17 +823,13 @@ library(ggh4x)
                                                   "Sus scrofa"),"FALSE", "TRUE")) %>% 
     group_by(Peixe) %>% 
     summarize("Peixe" = unique(Peixe),
-              #`Sample` = unique(Sample),
-              #`Curated ID` = unique(`Curated ID`),
-              # `Expedition` = unique(Expedition),
-              # `Year` = unique(Year),
-              # `Point` = unique(Point),
-              # `Sample.Name` = unique(Sample.Name),
-              # `File_name` = unique(File_name),
-              `RRA` = sum(`Relative abundance on sample`)/11
-              # ,
-              # `RAAS` = (`Relative abundance on all samples`)
-              ) %>%
+              `RRA` = sum(`Relative abundance on sample`)/11,
+              `ASVs` = sum(`Abundance`),
+              `Proporcao` = sum(`ASVs` / 2730833 * 100)
+              ) %>% 
     ungroup() 
   
-## descobrir quanto que a abundancia representa em ASVs
+## Utilizando o resultado da proporcao para obter o numero de ASVs
+  grouped_by_ID_tbl <- grouped_by_ID_tbl %>% 
+    mutate("ASVs" = raw_results_tbl$Abundance * `RRA`)
+  
